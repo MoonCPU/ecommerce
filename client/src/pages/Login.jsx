0ac249c7747dc506ce5a2964677d2b06110467d9
MoonCPU <moonkang20@gmail.com>
2023-09-28 00:09:41 -0300

    addind notification to login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,41 @@
 import  { useEffect, useContext } from 'react';
 import axios from 'axios';
 import jwtDecode from 'jwt-decode'; // Import jwtDecode
-import { Link } from 'react-router-dom';
+import { Link, useNavigate  } from 'react-router-dom';
 import { AuthContext } from '../components/AuthProvider';
 
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
 function Login() {
-    const { user, setUser } = useContext(AuthContext);
+    const { setUser } = useContext(AuthContext);
+    const navigate = useNavigate()
+
+    const notifyLoginSuccess = () => {
+        toast.success('Logged in!', {
+            position: "top-right",
+            autoClose: 500,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+    }
+
+    const notifyLoginFail = () => {
+        toast.error('Login failed!', {
+            position: "top-right",
+            autoClose: 500,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
+    }
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -27,42 +57,52 @@ function Login() {
             localStorage.setItem('token', token);
             const decodedToken = jwtDecode(token);
             setUser(decodedToken.user);
+            notifyLoginSuccess();
+            setTimeout(() => {
+                return navigate("/");
+            }, 2000);
         } catch (error) {         
             console.log(error.message);
+            notifyLoginFail();
         }
     }
 
     return (
         <div className="max-w-md sm:max-w-xl mx-auto flex items-center justify-center">
-            {user ? (
-                <div className='flex flex-col items-center justify-center box-border p-2 text-lg sm:text-2xl text-center'>
-                    <h1>Welcome back!</h1>
-                    <h1>Directing you to home page...</h1>    
+            <form onSubmit={handleSubmit} className='mx-2 md:m-0 w-full p-5 sm:p-10 flex flex-col border-black shadow-lg dark:shadow-black/30'>
+                <h1 className="text-lg sm:text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                    Sign in to your account
+                </h1>
+                <div className="mt-4"> 
+                    <div className='my-2'>
+                        <label className='block mb-2 text-sm sm:text-lg font-medium text-gray-900'>Email:</label>
+                        <input type="email" name='email' required className="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@email.com" /> 
+                    </div>
+                    <div className='my-2'>
+                        <label className='block mb-2 text-sm sm:text-lg font-medium text-gray-900'>Password:</label>
+                        <input type="password" name='password' required className="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="****" /> 
+                    </div>
                 </div>
-            ): (
-                <form onSubmit={handleSubmit} className='mx-2 md:m-0 w-full p-5 sm:p-10 flex flex-col border-black shadow-lg dark:shadow-black/30'>
-                    <h1 className="text-lg sm:text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                        Sign in to your account
-                    </h1>
-                    <div className="mt-4"> 
-                        <div className='my-2'>
-                            <label className='block mb-2 text-sm sm:text-lg font-medium text-gray-900'>Email:</label>
-                            <input type="email" name='email' required className="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@email.com" /> 
-                        </div>
-                        <div className='my-2'>
-                            <label className='block mb-2 text-sm sm:text-lg font-medium text-gray-900'>Password:</label>
-                            <input type="password" name='password' required className="bg-gray-50 border border-gray-300 text-gray-900 text-sm sm:text-base rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="****" /> 
-                        </div>
-                    </div>
-                    <div className='flex items-center justify-center'>
-                        <button type="submit" className="mt-4 rounded-md bg-blue-500 hover:bg-blue-600 text-white w-full p-2 font-semibold">Login</button>
-                    </div>
-                    <div className='mt-2 text-sm sm:text-base'>
-                        <h2>Register new account <Link to="/register" className='font-medium text-gray-900 underline hover:text-green-700'>here</Link>
-                        </h2>
-                    </div>
-                </form>
-            )}
+                <div className='flex items-center justify-center'>
+                    <button type="submit" className="mt-4 rounded-md bg-blue-500 hover:bg-blue-600 text-white w-full p-2 font-semibold">Login</button>
+                </div>
+                <div className='mt-2 text-sm sm:text-base'>
+                    <h2>Register new account <Link to="/register" className='font-medium text-gray-900 underline hover:text-green-700'>here</Link>
+                    </h2>
+                </div>
+                <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                />
+            </form>
         </div>
     );
 }
