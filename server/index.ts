d12280aeb70e5e5695b0391bee420b9258deea29
MoonCPU <moonkang20@gmail.com>
2023-10-08 19:45:44 -0300

    cors buf fix try 1

--- server/index.ts
+++ server/index.ts
@@ -10,13 +10,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.options('*', cors());
-
 app.use(express.json());
 
 app.use(cors({
     origin: 'https://ecommerce-kappa-umber.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     credentials: true,
 }));
 
