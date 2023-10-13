6a29aaff2f813b06a183ace68f035020dc7e3261
MoonCPU <moonkang20@gmail.com>
2023-10-13 14:14:09 -0300

    cors fix

--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: process.env.REACT_URL || process.env.REACT_RENDER,
+    origin: process.env.REACT_URL,
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     credentials: true,
 }));
